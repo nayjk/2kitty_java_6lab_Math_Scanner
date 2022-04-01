fde51690759b601e569ecb74dc16b08b668aec7b
Nayjk <98636064+LobkovaLiza@users.noreply.github.com>
2022-04-01 11:52:16 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,35 @@
 package com.metanit;
 
 import java.util.Scanner;
-import java.util.Random;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите x");
         int x = in.nextInt();
         System.out.println("Введите Y");
         int y = in.nextInt();
-        double z;
-        class number{
-            public int x;
-            public int y;
-            public number(int x, int y){
-                this.x = x;
-                this.y = y;
+        number v = new number(x, y);
+        v.cat();
+    }
+}
+class number{
+    public int x;
+    public int y;
+    public number(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public void cat(){
+        if (x == 0){
+            System.out.println("Переменная равна нулю, на ноль делить нельзя");
+            if (y == 0){
+                System.out.println("Переменная равна нулю, на ноль делить нельзя");
             }
-
         }
-        double a = x + y;
-        double b = 1 + y;
-        if (b == 0){
-            System.out.println("Знаменатель равен нулю, на ноль делить нельзя");
-        }
-        else{
-            z = a/b;
-            System.out.println("Получилось " + z);
-        }
+        double a = x + y; //знаменат
+        double b = 1 + y; //числит
+        double z = a/b;
+        System.out.println("Получилось " + z );
     }
 }
